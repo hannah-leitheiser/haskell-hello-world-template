e340a629f8ab6e7a6ab90c6d8d57d975f3826a19
hannah-leitheiser <leitheiserhannah@runbox.com>
2024-06-16 04:21:29 -0500

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -3,16 +3,8 @@ import Data.Text.I18n.Po
 import qualified Data.Text as T
 import System.Environment (lookupEnv)
 
+import HelloWorld
+
 main :: IO ()
 main = do
-  let localeDir = "resources/locale"
-
-  -- Try to get the system locale
-  maybeLocale <- lookupEnv "LANG"
-  let locale = case maybeLocale of
-                 Just loc -> takeWhile (/= '.') loc  -- Strip off any encoding info, e.g., ".UTF-8"
-                 Nothing  -> "en"  -- Default to "en" if locale is not found
-  
-  (l10n, _) <- getL10n localeDir
-  let message = T.unpack ( localize l10n (Locale (T.pack locale)) (gettext (T.pack "hello world") ) )
-  putStrLn (message )
+  greetWorld
