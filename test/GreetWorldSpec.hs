49779fa1e7141c64d795c48ba949e879a9f3f4ab
hannah-leitheiser <leitheiserhannah@runbox.com>
2024-06-16 07:07:24 -0500

    Create GreetWorldSpec.hs

---
+++ test/GreetWorldSpec.hs
@@ -0,0 +1,12 @@
+module GreetWorldSpec (spec) where
+
+import Test.Hspec
+
+import qualified GreetWorld
+
+spec :: Spec
+spec = describe "GreetWorld" $ do
+  it "greets the world in English" $ do
+    message -> GreetWorld.greetWorld "en" 
+    message `shouldBe` "Hello, World!"
+  -- Add more test cases as needed
