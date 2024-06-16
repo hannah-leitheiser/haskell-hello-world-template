f8330b62c23ca0e20ee10e5650b57aebfbc38180
hannah-leitheiser <leitheiserhannah@runbox.com>
2024-06-16 04:43:37 -0500

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,4 +2,9 @@ import HelloWorld
 
 main :: IO ()
 main = do
-  greetWorld
+  -- Try to get the system locale
+  maybeLocale <- lookupEnv "LANG"
+  let locale = case maybeLocale of
+                 Just loc -> takeWhile (/= '.') loc  -- Strip off any encoding info, e.g., ".UTF-8"
+                 Nothing  -> "en"  -- Default to "en" if locale is not found
+  putStrLn (greetWorld locale)
