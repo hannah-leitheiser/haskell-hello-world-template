fcd3449cc2782fadca1d31c3aa8b22d9681e037a
hannah-leitheiser <leitheiserhannah@runbox.com>
2024-06-16 04:39:09 -0500

    Update Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+import Test.Tasty
+import Test.Tasty.Hspec
+import HelloWorld (greetWorld)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  tests <- testSpec "HelloWorld tests" spec
+  defaultMain tests
+
+spec :: Spec
+spec = describe "HelloWorld" $
+  it "greets the world" $
+    greetWorld `shouldBe` "hello world"
